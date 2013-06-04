ec3d17dc80c1d9bdc40ee4fbdf5aaf3c5fb91eb6
Manfred Stienstra <manfred@fngtps.com>
2013-06-04 21:12:10 +0200

    [App] Set expiration time for newly instantiated sessions.

--- app/models/session.rb
+++ app/models/session.rb
@@ -5,7 +5,10 @@ require 'app/models/owner'
 module Pod
   module PushApp
     class Session < Sequel::Model
+      SECONDS_IN_DAY = 3600 * 24
+
       DEFAULT_TOKEN_LENGTH = 32 # characters
+      DEFAULT_VALIDITY_LENGTH = 128 # days
 
       self.dataset = :owners
       plugin :timestamps
@@ -13,16 +16,23 @@ module Pod
       many_to_one :owner, :class => 'Pod::PushApp::Owner'
 
       attr_accessor :token_length
+      attr_reader :valid_for
 
       def after_initialize
         super
         set_defaults
       end
 
+      def valid_for=(duration_in_days)
+        @valid_for = duration_in_days
+        self.valid_until = Time.now + (duration_in_days * SECONDS_IN_DAY)
+      end
+
       private
 
       def set_defaults
         if new?
+          self.valid_for ||= DEFAULT_VALIDITY_LENGTH
           self.token_length ||= DEFAULT_TOKEN_LENGTH
           set_token
         end
--- spec/unit/session_spec.rb
+++ spec/unit/session_spec.rb
@@ -10,5 +10,24 @@ module Pod::PushApp
       session = Session.new(:token_length => 2)
       session.token.length.should == 2
     end
+
+    it "has a default validity time" do
+      Session.new.valid_for.should == 128
+    end
+
+    it "sets a default expiration date" do
+      expected = Time.now + Session.new.valid_for * 3600 * 24
+      Session.new.valid_until.to_s.should == expected.to_s
+    end
+
+    it "allows you to set a different validity time" do
+      Session.new(:valid_for => 7).valid_for.should == 7
+    end
+
+    it "sets a default expiration date based on the validity time" do
+      valid_for = 23
+      expected = Time.now + valid_for * 3600 * 24
+      Session.new(:valid_for => valid_for).valid_until.to_s.should == expected.to_s
+    end
   end
 end
