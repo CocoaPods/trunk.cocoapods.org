06df35c6d1fae661cd7b87da96cd2c7f5025a7a0
Manfred Stienstra <manfred@fngtps.com>
2013-06-04 20:27:08 +0200

    [App] Add a session model.

---
+++ app/models/session.rb
@@ -0,0 +1,12 @@
+require 'app/models/owner'
+
+module Pod
+  module PushApp
+    class Session < Sequel::Model
+      self.dataset = :owners
+      plugin :timestamps
+
+      many_to_one :owner, :class => 'Pod::PushApp::Owner'
+    end
+  end
+end
---
+++ spec/unit/session_spec.rb
@@ -0,0 +1,6 @@
+require File.expand_path('../../spec_helper', __FILE__)
+
+module Pod::PushApp
+  describe "Session" do
+  end
+end
