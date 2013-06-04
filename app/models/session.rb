6bf1fa6db15440c4a7f1aab73abebdc6ea10e985
Manfred Stienstra <manfred@fngtps.com>
2013-06-04 21:05:35 +0200

    [App] Generate token for newly instantiated sessions.

--- app/models/session.rb
+++ app/models/session.rb
@@ -5,10 +5,32 @@ require 'app/models/owner'
 module Pod
   module PushApp
     class Session < Sequel::Model
+      DEFAULT_TOKEN_LENGTH = 32 # characters
+
       self.dataset = :owners
       plugin :timestamps
 
       many_to_one :owner, :class => 'Pod::PushApp::Owner'
+
+      attr_accessor :token_length
+
+      def after_initialize
+        super
+        set_defaults
+      end
+
+      private
+
+      def set_defaults
+        if new?
+          self.token_length ||= DEFAULT_TOKEN_LENGTH
+          set_token
+        end
+      end
+
+      def set_token
+        self.token = Token.generate(:length => token_length)
+      end
     end
   end
 end
--- spec/unit/session_spec.rb
+++ spec/unit/session_spec.rb
@@ -2,5 +2,13 @@ require File.expand_path('../../spec_helper', __FILE__)
 
 module Pod::PushApp
   describe "Session" do
+    it "automatically creates a token for itself" do
+      Session.new.token.length.should == 32
+    end
+
+    it "allows you to configure the token length" do
+      session = Session.new(:token_length => 2)
+      session.token.length.should == 2
+    end
   end
 end
