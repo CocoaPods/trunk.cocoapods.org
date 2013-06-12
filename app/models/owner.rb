be10ccb6dc82e70d35d26f4334a49d022c0f372a
Manfred Stienstra <manfred@fngtps.com>
2013-06-12 22:05:09 +0200

    [App] Add methods to coerce an Owner to YAML.

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -5,6 +5,34 @@ module Pod
       plugin :timestamps
 
       one_to_many :sessions, :class => 'Pod::PushApp::Session'
+
+      def attributes
+        values.inject({}) do |hash, (key, value)|
+          hash[key.to_s] = value
+          hash
+        end
+      end
+
+      def public_attributes
+        attributes
+      end
+
+      def to_yaml
+        public_attributes.to_yaml
+      end
+
+      def self.normalize_email(email)
+        email.to_s.strip.downcase
+      end
+
+      def self.find_or_create_by_email(email)
+        email = normalize_email(email)
+        if owner = where('email = ?', email).first
+          owner
+        else
+          create(:email => email)
+        end
+      end
     end
   end
 end
--- spec/unit/owner_spec.rb
+++ spec/unit/owner_spec.rb
@@ -2,5 +2,13 @@ require File.expand_path('../../spec_helper', __FILE__)
 
 module Pod::PushApp
   describe "Owner" do
+    before do
+      @owner = Owner.create(:email => 'jenny@example.com', :name => 'Jenny')
+    end
+
+    it "coerces to YAML" do
+      yaml = YAML.load(@owner.to_yaml)
+      yaml.keys.sort.should == %w(created_at email id name)
+    end
   end
 end
