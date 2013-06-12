560335cbd80017dc6e1b9f7d811529ba7b25fa25
Manfred Stienstra <manfred@fngtps.com>
2013-06-12 22:12:11 +0200

    [App] Make sure find or create for owners works properly.

--- spec/unit/owner_spec.rb
+++ spec/unit/owner_spec.rb
@@ -10,5 +10,31 @@ module Pod::PushApp
       yaml = YAML.load(@owner.to_yaml)
       yaml.keys.sort.should == %w(created_at email id name)
     end
+
+    it "finds itself with an email address" do
+      owner = Owner.find_or_create_by_email(@owner.email)
+      owner.should.not.be.new
+      owner.email.should == @owner.email
+    end
+
+    it "creates itself with an email address" do
+      email = 'janny@example.com'
+      owner = Owner.find_or_create_by_email(email)
+      owner.should.not.be.new
+      owner.email.should == email
+    end
+
+    it "normalizes the email address when finding by email address" do
+      owner = Owner.find_or_create_by_email(" #{@owner.email.upcase} ")
+      owner.should.not.be.new
+      owner.email.should == @owner.email
+    end
+
+    it "normalizes the email address when creating by email address" do
+      email = 'janny@example.com'
+      owner = Owner.find_or_create_by_email(" #{email.upcase} ")
+      owner.should.not.be.new
+      owner.email.should == email
+    end
   end
 end
