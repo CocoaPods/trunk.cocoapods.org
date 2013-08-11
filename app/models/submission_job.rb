5663935952ec7dade54d596c08d4a8cc188f2044
Eloy Durán <eloy.de.enige@gmail.com>
2013-08-11 23:00:45 +0200

    [migration] Chicken and egg problem...

--- app/models/submission_job.rb
+++ app/models/submission_job.rb
@@ -134,7 +134,11 @@ module Pod
       end
 
       def travis_build_url
-        Travis.web_url_for_id(travis_build_id) if travis_build_id
+        if defined?(super)
+          super
+        else
+          Travis.web_url_for_id(travis_build_id) if travis_build_id
+        end
       end
 
       def attempts=(count)
