54531a8551887aa220fc201aecdfb9f236b410fd
Eloy Durán <eloy.de.enige@gmail.com>
2013-08-13 21:55:38 +0200

    Move registration specs into their own describe block.

--- spec/functional/api_controller_spec.rb
+++ spec/functional/api_controller_spec.rb
@@ -140,7 +140,32 @@ EOYAML
     end
   end
 
-  describe APIController, "an unauthenticated consumer, which is not known to the system" do
+  describe APIController, "an unauthenticated consumer" do
+    before do
+      @email = 'jenny@example.com'
+      header 'Content-Type', 'text/yaml'
+    end
+
+    it "is not allowed to post a new pod" do
+      spec = fixture_specification('AFNetworking.podspec')
+      lambda {
+        lambda {
+          post '/pods', spec.to_yaml
+        }.should.not.change { Pod.count }
+      }.should.not.change { PodVersion.count }
+      last_response.status.should == 401
+    end
+
+    it "is allowed to GET status of a pod version" do
+      spec = fixture_specification('AFNetworking.podspec')
+      version = Pod.create(:name => spec.name).add_version(:name => spec.version.to_s)
+      version.add_submission_job(:specification_data => spec.to_yaml, :pull_request_number => 3)
+      get '/pods/AFNetworking/versions/1.2.0'
+      last_response.status.should == 202
+    end
+  end
+
+  describe APIController, "concerning registration" do
     extend SpecHelpers::Response
 
     before do
@@ -160,24 +185,6 @@ EOYAML
       post '/register', { 'email' => @email }.to_yaml
       last_response.status.should == 201
       yaml_response['email'].should == @email
-    end
-
-    it "is not allowed to post a new pod" do
-      spec = fixture_specification('AFNetworking.podspec')
-      lambda {
-        lambda {
-          post '/pods', spec.to_yaml
-        }.should.not.change { Pod.count }
-      }.should.not.change { PodVersion.count }
-      last_response.status.should == 401
-    end
-
-    it "is allowed to GET status of a pod version" do
-      spec = fixture_specification('AFNetworking.podspec')
-      version = Pod.create(:name => spec.name).add_version(:name => spec.version.to_s)
-      version.add_submission_job(:specification_data => spec.to_yaml, :pull_request_number => 3)
-      get '/pods/AFNetworking/versions/1.2.0'
-      last_response.status.should == 202
     end
   end
 
