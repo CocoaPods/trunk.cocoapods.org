6b8638b98144cda8c4091a70288c866ba486ff7e
Eloy Durán <eloy.de.enige@gmail.com>
2013-08-18 19:10:06 +0200

    [Pod] Add callback in case a pod is request that is not owned by the specified owner.

--- app/models/pod.rb
+++ app/models/pod.rb
@@ -9,12 +9,22 @@ module Pod
       one_to_many :versions, :class => 'Pod::TrunkApp::PodVersion'
       many_to_many :owners
 
-      def self.find_by_name_and_owner(name, owner, create_if_non_existing = false)
+      # Finds a pod by name.
+      #
+      # If the owner does not have access to the pod, the block is yielded.
+      def self.find_by_name_and_owner(name, owner)
         if pod = find(:name => name)
-          pod if pod.owners.include?(owner)
-        elsif create_if_non_existing
-          owner.add_pod(:name => name)
+          if pod.owners.include?(owner)
+            return pod
+          else
+            yield if block_given?
+          end
         end
+        nil
+      end
+
+      def self.find_or_create_by_name_and_owner(name, owner, &block)
+        find_by_name_and_owner(name, owner, &block) || owner.add_pod(:name => name)
       end
     end
   end
--- spec/unit/pod_spec.rb
+++ spec/unit/pod_spec.rb
@@ -16,38 +16,46 @@ module Pod::TrunkApp
 
     it "does not create a pod if it doesn't exist yet" do
       pod = nil
+      yielded = false
       lambda {
-        pod = Pod.find_by_name_and_owner('AFNetworking', @owner)
+        pod = Pod.find_by_name_and_owner('AFNetworking', @owner) { yielded = true; nil }
       }.should.not.change { Pod.count }
       pod.should == nil
+      yielded.should == false
     end
 
     it "creates a pod if it did not exist yet" do
       pod = nil
+      yielded = false
       lambda {
-        pod = Pod.find_by_name_and_owner('AFNetworking', @owner, true)
+        pod = Pod.find_or_create_by_name_and_owner('AFNetworking', @owner) { yielded = true; nil }
       }.should.change { Pod.count }
       pod.name.should == 'AFNetworking'
       pod.owners.should == [@owner]
+      yielded.should == false
     end
 
     it "returns an existing pod if it's owned by the specified owner" do
       pod = @owner.add_pod(:name => 'AFNetworking')
       found_pod = nil
+      yielded = false
       lambda {
-        found_pod = Pod.find_by_name_and_owner('AFNetworking', @owner)
+        found_pod = Pod.find_by_name_and_owner('AFNetworking', @owner) { yielded = true; nil }
       }.should.not.change { Pod.count }
       found_pod.should == pod
+      yielded.should == false
     end
 
     it "does not return a pod if it's not owned by the specified owner" do
       other_owner = Owner.create(:email => 'appie@example.com')
       other_owner.add_pod(:name => 'AFNetworking')
       found_pod = nil
+      yielded = false
       lambda {
-        found_pod = Pod.find_by_name_and_owner('AFNetworking', @owner)
+        found_pod = Pod.find_by_name_and_owner('AFNetworking', @owner) { yielded = true; nil }
       }.should.not.change { Pod.count }
       found_pod.should == nil
+      yielded.should == true
     end
   end
 end
