6a2e0d16018fc6f8c4ea8c06e27d7b50f256deb6
Eloy Durán <eloy.de.enige@gmail.com>
2013-08-18 16:22:32 +0200

    [Travis] Disable data migration which is no longer really needed.

--- db/migrations/022_migrate_travis_build_url_to_travis_build_id_on_submission_jobs.rb
+++ db/migrations/022_migrate_travis_build_url_to_travis_build_id_on_submission_jobs.rb
@@ -1,17 +1,20 @@
-require 'app/models/submission_job'
+# This just migrated data, but it's doubtful it's useful to anyone and it
+# causes issues on fresh setups, so disabling it.
 
-Sequel.migration do
-  up do
-    Pod::TrunkApp::SubmissionJob.each do |job|
-      if job.travis_build_url
-        job[:travis_build_id] = File.basename(job.travis_build_url).to_i
-        job.save
-      end
-    end
-  end
+#require 'app/models/submission_job'
 
-  down do
-  end
-end
+#Sequel.migration do
+  #up do
+    #Pod::TrunkApp::SubmissionJob.each do |job|
+      #if job.travis_build_url
+        #job[:travis_build_id] = File.basename(job.travis_build_url).to_i
+        #job.save
+      #end
+    #end
+  #end
+
+  #down do
+  #end
+#end
 
 
