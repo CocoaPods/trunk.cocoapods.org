1cbdcc7fda9961cce0756340f01cba4d36d52231
Eloy Durán <eloy.de.enige@gmail.com>
2014-02-12 21:38:00 +0100

    [Claims] Refactor controller code.

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -19,42 +19,58 @@ module Pod
       end
 
       post '/' do
+        find_owner
+        find_pods
+        if @owner.valid? && !@pods.empty? && @invalid_pods.empty?
+          change_ownership
+          redirect to('/thanks')
+        else
+          prepare_errors
+          slim :'new'
+        end
+      end
+
+      private
+
+      def find_owner
         owner_email, owner_name = params[:owner].values_at('email', 'name')
         @owner = Owner.find_or_initialize_by_email_and_update_name(owner_email, owner_name)
+      end
 
+      def find_pods
         @pods = []
-        invalid_pods = []
+        @invalid_pods = []
         unless params[:pods].blank?
           params[:pods].map(&:strip).uniq.each do |pod_name|
             next if pod_name.blank?
             if pod = Pod.find(:name => pod_name)
               @pods << pod
             else
-              invalid_pods << pod_name
+              @invalid_pods << pod_name
             end
           end
         end
+      end
 
-        if @owner.valid? && !@pods.empty? && invalid_pods.empty?
-          DB.test_safe_transaction do
-            @owner.save_changes(:raise_on_save_failure => true)
-            unclaimed_owner = Owner.unclaimed
-            @pods.each do |pod|
-              if pod.owners == [unclaimed_owner]
-                @owner.add_pod(pod)
-                pod.remove_owner(unclaimed_owner)
-              end
+      def prepare_errors
+        @errors = @owner.errors.full_messages.map { |message| "Owner #{message}." }
+        if !@invalid_pods.empty?
+          @errors << "Unknown #{'Pod'.pluralize(@invalid_pods.size)} #{@invalid_pods.to_sentence}."
+        elsif @pods.empty?
+          @errors << 'No Pods specified.'
+        end
+      end
+
+      def change_ownership
+        DB.test_safe_transaction do
+          @owner.save_changes(:raise_on_save_failure => true)
+          unclaimed_owner = Owner.unclaimed
+          @pods.each do |pod|
+            if pod.owners == [unclaimed_owner]
+              @owner.add_pod(pod)
+              pod.remove_owner(unclaimed_owner)
             end
           end
-          redirect to('/thanks')
-        else
-          @errors = @owner.errors.full_messages.map { |message| "Owner #{message}." }
-          if !invalid_pods.empty?
-            @errors << "Unknown #{'Pod'.pluralize(invalid_pods.size)} #{invalid_pods.to_sentence}."
-          elsif @pods.empty?
-            @errors << 'No Pods specified.'
-          end
-          slim :'new'
         end
       end
 
