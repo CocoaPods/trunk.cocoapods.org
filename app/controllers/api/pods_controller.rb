c34dd74ebf102f730a01e5cacc21a65755054a8a
Florian R. Hanke <florian.hanke@gmail.com>
2014-02-14 15:47:56 +0100

    [PodsController] Fix action.

--- app/controllers/api/pods_controller.rb
+++ app/controllers/api/pods_controller.rb
@@ -22,7 +22,8 @@ module Pod
         if pod = Pod.find(:name => params[:name])
           if version = pod.versions_dataset.where(:name => params[:version]).first
             if version.published?
-              job = version.published_by
+              commit = version.last_pushed_by
+              job = commit.pushed_by
               json_message(200, 'messages' => job.log_messages.map(&:public_attributes),
                                 'data_url' => version.data_url)
             end
