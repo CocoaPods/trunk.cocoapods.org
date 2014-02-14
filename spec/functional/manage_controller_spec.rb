120c19c719cdaa7facd684933606fa59e353411b
Florian R. Hanke <florian.hanke@gmail.com>
2014-02-14 15:47:06 +0100

    [ManageController] Fix specs.

--- spec/functional/manage_controller_spec.rb
+++ spec/functional/manage_controller_spec.rb
@@ -23,36 +23,84 @@ module Pod::TrunkApp
     before do
       authorize 'admin', 'secret'
     end
-
+    
     it "shows a list of current submission jobs" do
-      @job.commit.update(:pushed => nil)
-      get '/jobs'
+      @commit.update(:pushed => nil)
+      get '/commits'
       last_response.should.be.ok
-      last_response.body.should.include @pod.name
+      last_response.body.should.include @commit.pod_version.name
     end
 
     it "shows a list of failed submission jobs" do
-      @job.commit.update(:pushed => false)
-      get '/jobs', :scope => 'failed'
+      @commit.update(:pushed => false)
+      get '/commits', :scope => 'failed'
       last_response.should.be.ok
-      last_response.body.should.include @pod.name
+      last_response.body.should.include @commit.pod_version.name
     end
 
     it "shows a list of succeeded submission jobs" do
-      @job.commit.update(:pushed => true)
-      get '/jobs', :scope => 'succeeded'
+      @commit.update(:pushed => true)
+      get '/commits', :scope => 'succeeded'
       last_response.should.be.ok
-      last_response.body.should.include @pod.name
+      last_response.body.should.include @commit.pod_version.name
     end
 
     it "shows a list of all submission jobs" do
       [nil, false, true].each do |scope|
-        @job.commit.update(:pushed => scope)
-        get '/jobs', :scope => 'all'
+        @commit.update(:pushed => scope)
+        get '/commits', :scope => 'all'
         last_response.should.be.ok
-        last_response.body.should.include @pod.name
+        last_response.body.should.include @commit.pod_version.name
       end
     end
+
+    it "shows an overview of an individual commit" do
+      @commit.update(:pushed => true)
+      get "/commits/#{@commit.id}"
+      last_response.should.be.ok
+      last_response.body.should.include @commit.pod_version.name
+    end
+
+    # TODO Progress bar for commits? What would that include? Probably doesn't make sense.
+    #
+    # it "redirects to an overview with a progress bar if the job is in progress" do
+    #   @job.commit.update(:pushed => nil)
+    #   get "/jobs/#{@job.id}"
+    #   last_response.should.be.redirect
+    #   last_response.location.should.end_with "/jobs/#{@job.id}?progress=true"
+    # end
+
+    # TODO Don't have an overview of jobs, but refer to /commits at the moment.
+    #
+    # it "shows a list of current submission jobs" do
+    #   @job.commit.update(:pushed => nil)
+    #   get '/jobs'
+    #   last_response.should.be.ok
+    #   last_response.body.should.include @pod.name
+    # end
+    # 
+    # it "shows a list of failed submission jobs" do
+    #   @job.commit.update(:pushed => false)
+    #   get '/jobs', :scope => 'failed'
+    #   last_response.should.be.ok
+    #   last_response.body.should.include @pod.name
+    # end
+    # 
+    # it "shows a list of succeeded submission jobs" do
+    #   @job.commit.update(:pushed => true)
+    #   get '/jobs', :scope => 'succeeded'
+    #   last_response.should.be.ok
+    #   last_response.body.should.include @pod.name
+    # end
+    # 
+    # it "shows a list of all submission jobs" do
+    #   [nil, false, true].each do |scope|
+    #     @job.commit.update(:pushed => scope)
+    #     get '/jobs', :scope => 'all'
+    #     last_response.should.be.ok
+    #     last_response.body.should.include @pod.name
+    #   end
+    # end
 
     it "shows an overview of an individual submission job" do
       @job.commit.update(:pushed => true)
