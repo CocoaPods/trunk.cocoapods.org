fa21087859e54ffb76233476ac377b107d5fe9d4
Eloy Durán <eloy.de.enige@gmail.com>
2014-02-16 14:43:40 +0100

    [Hooks] Remove email stubbing and create unclaimed owner once.

--- spec/functional/hooks_controller_spec.rb
+++ spec/functional/hooks_controller_spec.rb
@@ -46,11 +46,7 @@ module Pod::TrunkApp
     
       Pod.find(name: 'MobileAppTracker').should == nil
     end
-    
-    before do
-      RFC822.stubs(:mx_records).returns ['all good! :D']
-    end
-    
+
     it "processes payload data and does not do anything (if the pod version does not exist)" do
       # Create existing pod.
       #
@@ -76,10 +72,12 @@ module Pod::TrunkApp
       #
       pod.versions.find { |version| version.name == '1.0.1' }.commits.should == []
     end
-    
-    it "processes payload data and creates a new submission job (because the version exists)" do
-      Owner.create(:email => Owner::UNCLAIMED_OWNER_EMAIL, :name => 'Unclaimed')
 
+    before do
+      Owner.create(:email => Owner::UNCLAIMED_OWNER_EMAIL, :name => 'Unclaimed')
+    end
+
+    it "processes payload data and creates a new submission job (because the version exists)" do
       # Create existing pod.
       #
       existing_spec = ::Pod::Specification.from_json(fixture_read('GitHub/KFData.podspec.json'))
@@ -122,22 +120,21 @@ module Pod::TrunkApp
       existing_pod = Pod.create(:name => existing_spec.name)
       PodVersion.create(:pod => existing_pod, :name => existing_spec.version.version)
 
-      Owner.create(:email => Owner::UNCLAIMED_OWNER_EMAIL, :name => 'Unclaimed')
       test_user = Owner.create(:email => 'test.user@example.com', :name => 'Test User')
       test_user.add_pod(existing_pod)
 
       REST.stubs(:get).returns(rest_response.new(fixture_read('GitHub/KFData.podspec.json')))
-    
+
       header 'Content-Type', 'application/x-www-form-urlencoded'
       payload = fixture_read('GitHub/post_receive_hook_data.raw')
       post '/github-post-receive/', payload
-    
+
       last_response.status.should == 200
-      
+
       pod = Pod.find(name: 'KFData')
-      
+
       commit = pod.versions.last.last_published_by
-      
+
       commit.committer.should == test_user
     end
 
@@ -148,20 +145,18 @@ module Pod::TrunkApp
       existing_pod = Pod.create(:name => existing_spec.name)
       PodVersion.create(:pod => existing_pod, :name => existing_spec.version.version)
 
-      Owner.create(:email => Owner::UNCLAIMED_OWNER_EMAIL, :name => 'Unclaimed')
-      
       REST.stubs(:get).returns(rest_response.new(fixture_read('GitHub/KFData.podspec.json')))
-    
+
       header 'Content-Type', 'application/x-www-form-urlencoded'
       payload = fixture_read('GitHub/post_receive_hook_data.raw')
       post '/github-post-receive/', payload
-    
+
       last_response.status.should == 200
-      
+
       pod = Pod.find(name: 'KFData')
-      
+
       commit = pod.versions.last.last_published_by
-      
+
       commit.committer.should == Owner.unclaimed
     end
     
