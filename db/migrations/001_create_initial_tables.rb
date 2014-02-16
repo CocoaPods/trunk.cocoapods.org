becd581d5ff3f550d6e44f75920cb13888f08973
Florian R. Hanke <florian.hanke@gmail.com>
2014-02-16 12:35:28 +0100

    [LogMessage] Add level at DB level.

--- db/migrations/001_create_initial_tables.rb
+++ db/migrations/001_create_initial_tables.rb
@@ -44,10 +44,11 @@ Sequel.migration do
     
     create_table(:log_messages) do
       primary_key :id
+      String :level, :null=>false
       String :message, :text=>true, :null=>false
       DateTime :created_at
       DateTime :updated_at
-      Integer :push_job_id # If this ID is null, it is a global log message.
+      Integer :pod_version_id # If this ID is null, it is a global log message.
     end
     
     create_table(:pod_versions, :ignore_index_errors=>true) do
@@ -63,24 +64,15 @@ Sequel.migration do
     create_table(:commits) do
       primary_key :id
       String :specification_data, :text=>true, :null=>false
-      # TODO remove pushed field
-      TrueClass :pushed, :default=>nil # The single point of "has it been pushed"?
       String :sha, :size=>255 # This can be null if it hasn't been pushed yet.
       DateTime :created_at
       DateTime :updated_at
       foreign_key :pod_version_id, :pod_versions, :null=>false, :key=>[:id]
       foreign_key :committer_id, :owners, :null=>false, :key=>[:id] # We need to know who created the commit.
     end
-
-    create_table(:push_jobs) do
-      primary_key :id
-      DateTime :created_at
-      DateTime :updated_at
-      foreign_key :commit_id, :commits, :key=>[:id]
-    end
     
     alter_table(:log_messages) do
-      add_foreign_key [:push_job_id], :push_jobs, :name=>:log_messages_push_job_id_fkey, :key=>[:id]
+      add_foreign_key [:pod_version_id], :pod_versions, :name=>:log_messages_pod_version_id_fkey, :key=>[:id]
     end
   end
 end
