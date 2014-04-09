a3a8af9ab35bac651eb7dc469f61fb7b801cc141
Fabio Pelosin <fabiopelosin@gmail.com>
2014-04-09 18:54:32 +0200

    [ManageHelper] Refactor to resolve cyclomatic complexity

--- app/helpers/manage_helper.rb
+++ app/helpers/manage_helper.rb
@@ -1,38 +1,71 @@
 module Pod
   module TrunkApp
     module ManageHelper
+      # @return [String]
+      #
       def formatted_duration(seconds)
-        components = []
-        if seconds >= 60
-          minutes, seconds = time_components(seconds, 60)
-          if minutes >= 60
-            hours, minutes = time_components(minutes, 60)
-            if hours >= 24
-              days, hours = time_components(hours, 24)
-              components << formatted_time_component(days, 'day')
-              components << formatted_time_component(hours, 'hour') if hours > 0
-            else
-              components << formatted_time_component(hours, 'hour')
-              components << formatted_time_component(minutes, 'minute') if minutes > 0
-            end
-          else
-            components << formatted_time_component(minutes, 'minute')
-            components << formatted_time_component(seconds, 'second') if seconds > 0
+        unit = find_biggest_time_unit(seconds)
+        unit_count = seconds / unit_seconds(unit)
+        result = formatted_time_component(unit_count, unit)
+
+        remaining_seconds = seconds % unit_seconds(unit)
+        unless remaining_seconds.zero?
+          next_index = TIME_UNITS.index(unit) + 1
+          next_unit = TIME_UNITS[next_index]
+          next_unit_count = remaining_seconds / unit_seconds(next_unit)
+
+          unless next_unit_count.zero?
+            result << ' and '
+            result << formatted_time_component(next_unit_count, next_unit)
           end
-        else
-          components << formatted_time_component(seconds, 'second')
         end
-        components.join(' and ')
+        result
       end
 
       private
 
-      def time_components(duration, base)
-        [duration / base, duration % base]
+      # @return [Array] The time units sorted from biggest to smallest.
+      #
+      TIME_UNITS = [:day, :hour, :minute, :second]
+
+      # @return [Hash] The duration in seconds of each time unit.
+      #
+      TIME_UNIT_SECONDS = {
+        :day => 24 * 60 * 60,
+        :hour => 60 * 60,
+        :minute => 60,
+        :second => 1
+      }
+
+      def unit_seconds(unit)
+        TIME_UNIT_SECONDS[unit]
       end
 
+      # Returns the biggest time unit in the given time expressed in seconds.
+      #
+      # @param  [Fixnum] seconds The time expressed in seconds.
+      #
+      # @return [Symbol] The biggest unit.
+      #
+      def find_biggest_time_unit(seconds)
+        current = :second
+        TIME_UNITS.each do |unit|
+          unit_seconds = unit_seconds(unit)
+          if unit_seconds <= seconds
+            if unit_seconds > unit_seconds(current)
+              current = unit
+            end
+          end
+        end
+        current
+      end
+
+      # @return [String]
+      #
       def formatted_time_component(component, name)
-        "#{component} #{name.pluralize(component)}"
+        unless component.zero?
+          "#{component} #{name.to_s.pluralize(component)}"
+        end
       end
     end
   end
--- spec/unit/helpers/manage_helper_spec.rb
+++ spec/unit/helpers/manage_helper_spec.rb
@@ -4,12 +4,22 @@ module Pod::TrunkApp
   describe ManageHelper do
     extend ManageHelper
 
-    it "formats a duration that's given in seconds" do
+    it 'formats a duration which includes seconds' do
       formatted_duration(1).should == '1 second'
+      formatted_duration(59).should == '59 seconds'
+    end
+
+    it 'formats a duration which includes minutes' do
       formatted_duration(61).should == '1 minute and 1 second'
       formatted_duration(3599).should == '59 minutes and 59 seconds'
+    end
+
+    it 'formats a duration which includes hours' do
       formatted_duration(3601).should == '1 hour'
       formatted_duration(3660).should == '1 hour and 1 minute'
+    end
+
+    it 'formats a duration which includes days' do
       formatted_duration(3600 * 24).should == '1 day'
       formatted_duration(3600 * 25).should == '1 day and 1 hour'
     end
