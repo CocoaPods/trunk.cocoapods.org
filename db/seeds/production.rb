d08efb2784badee4729c23ba35353721d36de38f
Florian R. Hanke <florian.hanke@gmail.com>
2014-04-21 19:11:11 +0200

    [Rubocop] Silence Rubocop.
    Please put down your weapon. You have 20 seconds to comply.
    You now have 15 seconds to comply.
    You are in direct violation of Penal Code 1.13, Section 9.
    You have 5 seconds to comply.
    Four... three... two... one... I am now authorized to use physical force!

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -1,13 +1,15 @@
 require 'app/models/owner'
 
-module Pod::TrunkApp
-  # This is an insider attribution to those that have worked on the initial app.
-  Owner.create(:email => 'eloy.de.enige@gmail.com', :name => 'Eloy Durán')
-  Owner.create(:email => 'florian.hanke@gmail.com', :name => 'Florian R. Hanke')
-  Owner.create(:email => 'manfred@fngtps.com',      :name => 'Manfred Stienstra')
-  Owner.create(:email => 'orta.therox@gmail.com',   :name => 'Orta Therox')
-  Owner.create(:email => 'inbox@kylefuller.co.uk',  :name => 'Kyle Fuller')
-  Owner.create(:email => 'fabiopelosin@gmail.com',  :name => 'Fabio Pelosin')
+module Pod
+  module TrunkApp
+    # This is an insider attribution to those that have worked on the initial app.
+    Owner.create(:email => 'eloy.de.enige@gmail.com', :name => 'Eloy Durán')
+    Owner.create(:email => 'florian.hanke@gmail.com', :name => 'Florian R. Hanke')
+    Owner.create(:email => 'manfred@fngtps.com',      :name => 'Manfred Stienstra')
+    Owner.create(:email => 'orta.therox@gmail.com',   :name => 'Orta Therox')
+    Owner.create(:email => 'inbox@kylefuller.co.uk',  :name => 'Kyle Fuller')
+    Owner.create(:email => 'fabiopelosin@gmail.com',  :name => 'Fabio Pelosin')
 
-  Owner.create(:email => Owner::UNCLAIMED_OWNER_EMAIL, :name => 'Unclaimed')
+    Owner.create(:email => Owner::UNCLAIMED_OWNER_EMAIL, :name => 'Unclaimed')
+  end
 end
