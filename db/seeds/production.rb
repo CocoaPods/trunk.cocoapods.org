c3c93db54faf3a1c32722e48a7c980435535cd73
Eloy Durán <eloy.de.enige@gmail.com>
2014-04-21 18:32:13 +0200

    [seeds] Add attribution to all people that have worked on the trunk app.
    @floere @Manfred @orta @kylef @irrationalfab Please verify that these
    are the details you would normally use to push commits to GitHub.

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -1,7 +1,13 @@
 require 'app/models/owner'
 
 module Pod::TrunkApp
+  # This is an insider attribution to those that have worked on the initial app.
   Owner.create(:email => 'eloy.de.enige@gmail.com', :name => 'Eloy Durán')
+  Owner.create(:email => 'florian.hanke@gmail.com', :name => 'Florian R. Hanke')
+  Owner.create(:email => 'manfred@fngtps.com',      :name => 'Manfred Stienstra')
+  Owner.create(:email => 'orta.therox@gmail.com',   :name => 'Orta Therox')
+  Owner.create(:email => 'inbox@kylefuller.co.uk',  :name => 'Kyle Fuller')
+  Owner.create(:email => 'fabiopelosin@gmail.com',  :name => 'Fabio Pelosin')
 
   Owner.create(:email => Owner::UNCLAIMED_OWNER_EMAIL, :name => 'Unclaimed')
 end
