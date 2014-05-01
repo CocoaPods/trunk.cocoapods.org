5258fb9dac8e2217060af65891df7cd64be3cccd
Samuel E. Giddins <segiddins@segiddins.me>
2014-04-30 22:05:17 -0500

    [Spec] Change rest_response to return a REST::Response

--- spec/unit/commit/import_spec.rb
+++ spec/unit/commit/import_spec.rb
@@ -22,19 +22,21 @@ module Pod::TrunkApp
       )
     end
 
-    rest_response = Struct.new(:body)
+    def rest_response(body, code = 200, header = nil)
+      REST::Response.new(code, header, body)
+    end
 
     it 'gets the podspec data from the right URL' do
       expected_url = 'https://raw.github.com/CocoaPods/Specs/' \
         '3cc2186863fb4d8a0fd4ffd82bc0ffe88499bd5f/Specs/KFData/1.0.1/KFData.podspec.json'
       REST.expects(:get).with(expected_url).twice
-        .returns(rest_response.new(fixture_read('GitHub/ABContactHelper.podspec.json')))
+        .returns(rest_response(fixture_read('GitHub/ABContactHelper.podspec.json')))
 
       trigger_commit_with_fake_data
     end
 
     it 'processes payload data and creates a new pod (if one does not exist)' do
-      REST.stubs(:get).returns(rest_response.new(fixture_read('GitHub/ABContactHelper.podspec.json')))
+      REST.stubs(:get).returns(rest_response(fixture_read('GitHub/ABContactHelper.podspec.json')))
       lambda do
         trigger_commit_with_fake_data
       end.should.change { Pod.count }
@@ -58,7 +60,7 @@ module Pod::TrunkApp
     end
 
     it 'does add the add commit and a version if missing and version does not exist' do
-      REST.stubs(:get).returns(rest_response.new(fixture_read('GitHub/KFData.podspec.json')))
+      REST.stubs(:get).returns(rest_response(fixture_read('GitHub/KFData.podspec.json')))
       trigger_commit_with_fake_data
 
       # Did log a big fat warning.
@@ -73,7 +75,7 @@ module Pod::TrunkApp
     end
 
     it 'marks a commit as being imported' do
-      REST.stubs(:get).returns(rest_response.new(fixture_read('GitHub/KFData.podspec.json')))
+      REST.stubs(:get).returns(rest_response(fixture_read('GitHub/KFData.podspec.json')))
       trigger_commit_with_fake_data
       last_version = @existing_pod.reload.versions.last
       commit = last_version.last_published_by
@@ -87,7 +89,7 @@ module Pod::TrunkApp
     end
 
     it 'processes payload data and adds a new version, logs warning and commit (if the pod version does not exist)' do
-      REST.stubs(:get).returns(rest_response.new(fixture_read('GitHub/KFData.podspec.new.json')))
+      REST.stubs(:get).returns(rest_response(fixture_read('GitHub/KFData.podspec.new.json')))
 
       trigger_commit_with_fake_data
 
@@ -114,7 +116,7 @@ module Pod::TrunkApp
     # Stub data for the existing pod version
     #
     before do
-      REST.stubs(:get).returns(rest_response.new(fixture_read('GitHub/KFData.podspec.json')))
+      REST.stubs(:get).returns(rest_response(fixture_read('GitHub/KFData.podspec.json')))
     end
 
     it 'processes payload data and creates a new submission job (because the version exists)' do
@@ -216,7 +218,7 @@ module Pod::TrunkApp
       )
 
       PodVersion.any_instance.expects(:add_commit).never
-      REST.stubs(:get).returns(rest_response.new(fixture_read('GitHub/KFData.podspec.new.json')))
+      REST.stubs(:get).returns(rest_response(fixture_read('GitHub/KFData.podspec.new.json')))
 
       trigger_commit_with_fake_data
     end
