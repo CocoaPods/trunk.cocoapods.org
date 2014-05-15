24b33b0ffecf741fc054f118a1781b831aab512d
Florian R. Hanke <florian.hanke@gmail.com>
2014-05-15 20:44:38 +0200

    [Webhook] Add spec for time spent waiting for Webhook.call.

---
+++ spec/lib/webhook_spec.rb
@@ -0,0 +1,33 @@
+require File.expand_path('../../spec_helper', __FILE__)
+
+require 'webhook'
+
+describe 'Webhook' do
+
+  it 'does only block for a very short time' do
+    # Run webhooks in a child.
+    #
+    pid = fork do
+      Webhook.run
+    end
+
+    # Measure time waiting for webhook (blocks).
+    #
+    t = Time.now
+    Webhook.call('testing')
+    duration = Time.now - t
+
+    # Kill the child process.
+    #
+    Process.kill 'KILL', pid
+
+    # Collect the remains.
+    #
+    Process.waitall
+
+    # verify expectation.
+    #
+    duration.should < 0.005
+  end
+
+end
