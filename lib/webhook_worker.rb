23be9e0e1dd5ac6a435e7a42d87a1c6c32ee4671
Florian R. Hanke <florian.hanke@gmail.com>
2014-05-17 01:15:09 +0200

    [Webhook] Add request bin URL for manual testing.

--- lib/webhook_worker.rb
+++ lib/webhook_worker.rb
@@ -6,9 +6,13 @@ require 'lib/webhook'
 #
 garbled_hook_path = ENV['OUTGOING_HOOK_PATH']
 Webhook.urls = [
+  # For testing purposes.
+  #
+  "http://requestb.in/1d8wrju1"
+
   # "http://cocoadocs.org/hooks/trunk/#{garbled_hook_path}",
   # "http://metrics.cocoapods.org/hooks/trunk/#{garbled_hook_path}",
-  "http://search.cocoapods.org/hooks/trunk/#{garbled_hook_path}"
+  # "http://search.cocoapods.org/hooks/trunk/#{garbled_hook_path}"
 ]
 
 Webhook.run
