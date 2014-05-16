771516dc3dc154edff9353f5c750812efddd28c3
Florian R. Hanke <florian.hanke@gmail.com>
2014-05-17 01:20:25 +0200

    [RuboCop] Why do we have this thing?

--- lib/webhook_worker.rb
+++ lib/webhook_worker.rb
@@ -8,7 +8,7 @@ garbled_hook_path = ENV['OUTGOING_HOOK_PATH']
 Webhook.urls = [
   # For testing purposes.
   #
-  "http://requestb.in/1d8wrju1"
+  'http://requestb.in/1d8wrju1'
 
   # "http://cocoadocs.org/hooks/trunk/#{garbled_hook_path}",
   # "http://metrics.cocoapods.org/hooks/trunk/#{garbled_hook_path}",
