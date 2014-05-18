d3a14f0564fe2e64203081495630c2c112e1ce7f
Florian R. Hanke <florian.hanke@gmail.com>
2014-05-18 20:58:48 +0200

    [Webhook] Add cocoadocs to the URLs.

--- config/init.rb
+++ config/init.rb
@@ -38,8 +38,8 @@ require 'lib/webhook'
 # Warning: Do not add non-existing domains.
 #
 garbled_hook_path = ENV['OUTGOING_HOOK_PATH']
-Webhook.setup 'http://requestb.in/1d8wrju1'
-              # "http://cocoadocs.org/hooks/trunk/#{garbled_hook_path}",
+Webhook.setup 'http://requestb.in/1d8wrju1',
+              "http://199.229.252.197:4567/hooks/trunk/#{garbled_hook_path}",
               # "http://metrics.cocoapods.org/hooks/trunk/#{garbled_hook_path}",
               # "http://search.cocoapods.org/hooks/trunk/#{garbled_hook_path}"
 
