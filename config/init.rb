6c362d596daeebd2b3eece203a792493ac9fb0dc
Florian R. Hanke <florian.hanke@gmail.com>
2014-05-18 20:20:42 +0200

    [Webhook] Setup in init.rb.

--- config/init.rb
+++ config/init.rb
@@ -32,7 +32,16 @@ end
 # -- Webhooks -----------------------------------------------------------------
 
 require 'lib/webhook'
-Webhook.setup
+
+# List of attached web hook URLs.
+#
+# Warning: Do not add non-existing domains.
+#
+garbled_hook_path = ENV['OUTGOING_HOOK_PATH']
+Webhook.setup 'http://requestb.in/1d8wrju1'
+              # "http://cocoadocs.org/hooks/trunk/#{garbled_hook_path}",
+              # "http://metrics.cocoapods.org/hooks/trunk/#{garbled_hook_path}",
+              # "http://search.cocoapods.org/hooks/trunk/#{garbled_hook_path}"
 
 # -- Logging ------------------------------------------------------------------
 
