69f743749b5a413bd03094ac39c0f4768f5e9fbd
Florian R. Hanke <florian.hanke@gmail.com>
2014-05-24 01:55:40 +0200

    [Webhook] Use UTC time in tests.

--- spec/lib/webhook_spec.rb
+++ spec/lib/webhook_spec.rb
@@ -38,6 +38,7 @@ describe 'Webhook' do
     describe 'convenience methods' do
       describe 'with URLs' do
         before do
+          @time = Time.parse('2001-01-01 00:00:00 UTC')
           Webhook.pod_created = %w(pod_created_url1 pod_created_url2)
           Webhook.version_created = %w(version_created_url1 version_created_url2)
           Webhook.spec_updated = %w(spec_updated_url1 spec_updated_url2)
@@ -50,42 +51,43 @@ describe 'Webhook' do
         it 'pod_created calls call correctly' do
           Webhook.expects(:write_child).once.with(
             %Q(pod;create;{"type":"pod","action":"create","timestamp":"2001-01-01 00:00:00 ) +
-            %Q(+0100","data_url":"some_url"};pod_created_url1,pod_created_url2\n)
+            %Q(UTC","data_url":"some_url"};pod_created_url1,pod_created_url2\n)
           )
-          Webhook.pod_created(Time.parse('01-01-2001'), 'some_url')
+          Webhook.pod_created(@time, 'some_url')
         end
         it 'version_created calls call correctly' do
           Webhook.expects(:write_child).once.with(
             %Q(version;create;{"type":"version","action":"create","timestamp":"2001-01-01 00:00:00 ) +
-            %Q(+0100","data_url":"some_url"};version_created_url1,version_created_url2\n)
+            %Q(UTC","data_url":"some_url"};version_created_url1,version_created_url2\n)
           )
-          Webhook.version_created(Time.parse('01-01-2001'), 'some_url')
+          Webhook.version_created(@time, 'some_url')
         end
         it 'spec_updated calls call correctly' do
           Webhook.expects(:write_child).once.with(
             %Q(spec;update;{"type":"spec","action":"update","timestamp":"2001-01-01 00:00:00 ) +
-            %Q(+0100","data_url":"some_url"};spec_updated_url1,spec_updated_url2\n)
+            %Q(UTC","data_url":"some_url"};spec_updated_url1,spec_updated_url2\n)
           )
-          Webhook.spec_updated(Time.parse('01-01-2001'), 'some_url')
+          Webhook.spec_updated(@time, 'some_url')
         end
       end
       describe 'without URLs' do
         before do
+          @time = Time.parse('2001-01-01 00:00:00 +0000')
           Webhook.pod_created = []
           Webhook.version_created = []
           Webhook.spec_updated = []
         end
         it 'pod_created calls call correctly' do
           Webhook.expects(:write_child).never
-          Webhook.pod_created(Time.parse('01-01-2001'), 'some_url')
+          Webhook.pod_created(@time, 'some_url')
         end
         it 'version_created calls call correctly' do
           Webhook.expects(:write_child).never
-          Webhook.version_created(Time.parse('01-01-2001'), 'some_url')
+          Webhook.version_created(@time, 'some_url')
         end
         it 'spec_updated calls call correctly' do
           Webhook.expects(:write_child).never
-          Webhook.spec_updated(Time.parse('01-01-2001'), 'some_url')
+          Webhook.spec_updated(@time, 'some_url')
         end
       end
     end
