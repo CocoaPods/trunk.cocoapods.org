505c1a812d25dc660fe58144a38f1964fcc346e4
Florian R. Hanke <florian.hanke@gmail.com>
2014-05-24 18:29:17 +0200

    [Webhook] Do not URI encode message.

--- lib/webhook.rb
+++ lib/webhook.rb
@@ -129,8 +129,7 @@ class Webhook
         # Spawn a worker, then wait for it to finish.
         #
         if message && !targets.empty?
-          encoded_message = URI.encode(message)
-          cmd = %Q(curl -X POST -sfL --data "message=#{encoded_message}" --connect-timeout 1 --max-time 1 {#{targets}})
+          cmd = %Q(curl -X POST -sfL --data "message=#{message}" --connect-timeout 1 --max-time 1 {#{targets}})
           fork { exec cmd }
           Process.waitall
         end
