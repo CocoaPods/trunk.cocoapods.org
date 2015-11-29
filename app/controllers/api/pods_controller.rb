1a13a60531b9e48b535232f0ca6f242acdfbfcf1
Orta Therox <orta.therox@gmail.com>
2015-11-29 09:07:11 -0500

    [deletion] Maps out a deletion route, and subsequently sends the message on to GitHub

--- app/controllers/api/pods_controller.rb
+++ app/controllers/api/pods_controller.rb
@@ -197,6 +197,43 @@ module Pod
         redirect pod.versions.last.resource_path if verify_github_responses!(responses)
       end
 
+      delete '/:name/:version', :requires_owner => true do
+        pod = Pod.find_by_name_and_owner(params[:name], @owner) do
+          json_error(403, 'You are not allowed to delete this pod.')
+        end
+        unless pod
+          json_error(404, 'No pod found with the specified name.')
+        end
+
+        version = pod.versions_dataset.where(:name => params[:version]).first
+        unless version
+          json_error(404, 'No pod version found with the specified version.')
+        end
+
+        response = version.delete!(@owner)
+        if response(&:success?)
+          redirect pod.versions.last.resource_path
+        elsif response(&:failed_on_our_side?)
+          throw_internal_server_error!
+        elsif response(&:failed_on_their_side?)
+          # In case of a 5xx at GitHub’s side, this might not mean the commit
+          # didn’t get created, it can also indicate an error occurred while
+          # rendering the response, hence asking for some patience in case we
+          # still update the PodVersion with a new Commit from the GitHub
+          # post-commit hook.
+          #
+          # TODO: Ask GitHub if they have some form of transaction system in
+          # place that rolls back a commit in case an error occurs during
+          # response rendering.
+          json_error(500, 'An error occurred on GitHub’s side. Please check GitHub’s status at ' \
+                          'https://status.github.com and try again later in case the pod is ' \
+                          'still not deleted.')
+        elsif responses.any(&:failed_due_to_timeout?)
+          json_error(504, 'Calling the GitHub commit API timed out. Please check GitHub’s ' \
+                          'status at https://status.github.com and try again later.')
+        end
+      end
+
       patch '/:name/owners', :requires_owner => true do
         pod = Pod.find_by_name_and_owner(params[:name], @owner) do
           json_error(403, 'You are not allowed to add owners to this pod.')
