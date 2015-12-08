175b2e192014fb87bb6d780479455bb86cfa1828
Samuel Giddins <segiddins@segiddins.me>
2015-12-08 13:04:29 -0600

    [PodsController] Extract GitHub error handling

--- app/controllers/api/pods_controller.rb
+++ app/controllers/api/pods_controller.rb
@@ -9,6 +9,33 @@ module Pod
     class PodsController < APIController
       MINIMUM_COCOAPODS_VERSION = Version.new((ENV['TRUNK_MINIMUM_COCOAPODS_VERSION'] || '0.36.0').dup)
 
+      def verify_github_responses!(responses)
+        responses = Array(responses)
+        responses.all? do |response|
+          if response.success?
+            true
+          elsif response.failed_on_our_side?
+            throw_internal_server_error!
+          elsif response.failed_on_their_side?
+            # In case of a 5xx at GitHub’s side, this might not mean the commit
+            # didn’t get created, it can also indicate an error occurred while
+            # rendering the response, hence asking for some patience in case we
+            # still update the PodVersion with a new Commit from the GitHub
+            # post-commit hook.
+            #
+            # TODO: Ask GitHub if they have some form of transaction system in
+            # place that rolls back a commit in case an error occurs during
+            # response rendering.
+            json_error(500, 'An error occurred on GitHub’s side. Please check GitHub’s status at ' \
+                            'https://status.github.com and try again later in case the pod is ' \
+                            'still not published.')
+          elsif response.failed_due_to_timeout?
+            json_error(504, 'Calling the GitHub commit API timed out. Please check GitHub’s ' \
+                            'status at https://status.github.com and try again later.')
+          end
+        end
+      end
+
       get '/:name', :requires_owner => false do
         if pod = Pod.find_by_name(params[:name])
           versions = pod.versions.select(&:published?)
@@ -138,27 +165,7 @@ module Pod
         end
 
         response = version.push!(@owner, specification.to_pretty_json, 'Add')
-        if response.success?
-          redirect url(version.resource_path)
-        elsif response.failed_on_our_side?
-          throw_internal_server_error!
-        elsif response.failed_on_their_side?
-          # In case of a 5xx at GitHub’s side, this might not mean the commit
-          # didn’t get created, it can also indicate an error occurred while
-          # rendering the response, hence asking for some patience in case we
-          # still update the PodVersion with a new Commit from the GitHub
-          # post-commit hook.
-          #
-          # TODO: Ask GitHub if they have some form of transaction system in
-          # place that rolls back a commit in case an error occurs during
-          # response rendering.
-          json_error(500, 'An error occurred on GitHub’s side. Please check GitHub’s status at ' \
-                          'https://status.github.com and try again later in case the pod is ' \
-                          'still not published.')
-        elsif response.failed_due_to_timeout?
-          json_error(504, 'Calling the GitHub commit API timed out. Please check GitHub’s ' \
-                          'status at https://status.github.com and try again later.')
-        end
+        redirect url(version.resource_path) if verify_github_responses!(response)
       end
 
       patch '/:name/deprecated', :requires_owner => true do
@@ -176,7 +183,7 @@ module Pod
         end
 
         if in_favor_of = deprecated_params['in_favor_of']
-          unless Pod.find_by_name(in_favor_of)
+          unless Pod.find_by_name(Specification.root_name in_favor_of)
             json_error(422, 'You cannot deprecate a pod in favor of a pod that does not exist.')
           end
         end
@@ -186,27 +193,7 @@ module Pod
           json(422, 'There were no published versions to deprecate.')
         end
 
-        if responses.all(&:success?)
-          redirect pod.versions.last.resource_path
-        elsif responses.any?(&:failed_on_our_side?)
-          throw_internal_server_error!
-        elsif responses.any?(&:failed_on_their_side?)
-          # In case of a 5xx at GitHub’s side, this might not mean the commit
-          # didn’t get created, it can also indicate an error occurred while
-          # rendering the response, hence asking for some patience in case we
-          # still update the PodVersion with a new Commit from the GitHub
-          # post-commit hook.
-          #
-          # TODO: Ask GitHub if they have some form of transaction system in
-          # place that rolls back a commit in case an error occurs during
-          # response rendering.
-          json_error(500, 'An error occurred on GitHub’s side. Please check GitHub’s status at ' \
-                          'https://status.github.com and try again later in case the pod is ' \
-                          'still not published.')
-        elsif responses.any(&:failed_due_to_timeout?)
-          json_error(504, 'Calling the GitHub commit API timed out. Please check GitHub’s ' \
-                          'status at https://status.github.com and try again later.')
-        end
+        redirect pod.versions.last.resource_path if verify_github_responses!(responses)
       end
 
       patch '/:name/owners', :requires_owner => true do
