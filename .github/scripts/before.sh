0b405d9826c0673d784d248b0457f194f0b74131
Dimitris Koutsogiorgas <dnkoutso@gmail.com>
2021-09-01 10:55:28 -0700

    tmp

--- .github/scripts/before.sh
+++ .github/scripts/before.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/CocoaPods/Humus.git
+git clone --branch bump_ruby https://github.com/CocoaPods/Humus.git
 cd Humus
 bundle install
 RACK_ENV=test bundle exec rake db:bootstrap
