6c1fbe27dd19ce248b70e23bc378c8329c1d15f1
Samuel Giddins <segiddins@squareup.com>
2022-04-08 15:03:49 -0700

    Fix

--- spec/spec_helper/commit_response.rb
+++ spec/spec_helper/commit_response.rb
@@ -1,8 +1,8 @@
 module SpecHelpers
   module CommitResponse
-    def response(status = nil, body = nil, &)
+    def response(status = nil, body = nil, &block)
       block ||= lambda { REST::Response.new(status, {}, body) }
-      Pod::TrunkApp::GitHub::CommitResponse.new(&)
+      Pod::TrunkApp::GitHub::CommitResponse.new(&block)
     end
   end
 end
